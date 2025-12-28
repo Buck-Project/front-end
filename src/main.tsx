1be0385d2f8bd3ca28ca499574e9e5123ea2b4b0
it-hosein <hosseinmajidi7723@gmail.com>
2025-12-28 04:36:06 +0330

    persian number type

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { router } from "./routes/routes.tsx";
 import { RouterProvider } from "react-router-dom";
+import PersianDigitNormalizer from "./components/PersianDigitNormalizer";
 
 createRoot(document.getElementById("root")!).render(
-	<RouterProvider router={router} />
+	<>
+		<PersianDigitNormalizer />
+		<RouterProvider router={router} />
+	</>
 );
