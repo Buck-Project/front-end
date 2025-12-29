a7e8e9c8fdb40df4450e9b8caef25a62d3ae870a
it-hosein <hosseinmajidi7723@gmail.com>
2025-12-30 02:29:32 +0330

    edit

--- src/components/ProductPage/ProductPage.tsx
+++ src/components/ProductPage/ProductPage.tsx
@@ -603,12 +603,12 @@ const ProductTabs: React.FC<{
                                     <StarRating value={reviewRating} onChange={setReviewRating} />
                                 </div>
                                 <Input
-                                    placeholder="...نظر خود را ثبت کنید"
+                                    placeholder="نظر خود را ثبت کنید ..."
                                     value={reviewText}
                                     onChange={(e) => setReviewText(e.target.value)}
-                                    className="mb-4"
+                                    className="mb-4 rounded-[30px] placeholder:text-gray-400"
                                 />
-                                <Button onClick={submitReview}>ثبت نظر جدید</Button>
+                                <Button onClick={submitReview} className="mt-2 bg-[#E4004B] text-white rounded-[30px] hover:bg-[#E4004B]/90">{"\u062b\u0628\u062a \u0646\u0638\u0631 \u062c\u062f\u06cc\u062f"}</Button>
                             </CardContent>
                         </Card>
 
@@ -662,7 +662,7 @@ const ProductTabs: React.FC<{
                             </CardHeader>
                             <CardContent>
                                 <Input
-                                    placeholder="...سوال خود را بپرسید"
+                                    placeholder="\u0646\u0638\u0631 \u062e\u0648\u062f \u0631\u0627 \u062b\u0628\u062a \u06a9\u0646\u06cc\u062f ..."
                                     value={questionText}
                                     onChange={(e) => setQuestionText(e.target.value)}
                                     className="mb-4"
