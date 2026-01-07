bd2d0acf526c93794403a46b1b05cee4cb088c54
amrayi <amirmohammadamrayi@gmail.com>
2026-01-07 14:00:16 +0330

    edit route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -10,7 +10,7 @@ import LoginForm from "@/pages/Login";
 import Validation from "@/pages/Validation";
 import BrandProfileEditPage from "@/pages/BrandProfileEditPage";
 import WishlistPage from "@/pages/WishList";
-import ProductManagementPage from "@/pages/BrandDash/ProductManagementPage";
+import ProductManagementPage from "@/pages/BrandDash/ProductManagement";
 import OrderHistoryPage from "@/pages/OrderHistoryPage";
 import UserDashInformation from "@/pages/userDashInfo";
 import FAQ from "@/pages/FAQ";
